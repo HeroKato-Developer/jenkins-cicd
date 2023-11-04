cf339ca217d2dfeae1b971efd87b62c9fca88f26
Marco Concato <herokato.gamer@gmail.com>
2023-11-04 07:49:51 +0100

    req good

--- src/ServiceStart.ts
+++ src/ServiceStart.ts
@@ -1,8 +1,13 @@
+import { LoggerManager } from "evg-service-base";
+
 const express = require("express");
 const PORT = 8000;
 const app = express();
-app.get("/", (req, res) => {
+app.get("/", (req, res) =>
+{
     res.send("Hello Guys!!");
 });
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
+
+LoggerManager.WithLogger(() => { console.log('asd') })
