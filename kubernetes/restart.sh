7830cf9d7356bcab75934f3c7292349961797c2a
Marco Concato <herokato.gamer@gmail.com>
2023-11-04 05:30:57 +0100

    Update restart.sh

--- kubernetes/restart.sh
+++ kubernetes/restart.sh
@@ -1,1 +1,1 @@
-sshpass -p 'kato00ftw' ssh -o StrictHostKeyChecking=no kato@cryptoarena.app "microk8s kubectl rollout restart -n devs-ops deployment test-node"
+sshpass -p 'kato00ftw' ssh -o StrictHostKeyChecking=no kato@cryptoarena.app "microk8s kubectl rollout restart -n devops-tools deployment test-node"
